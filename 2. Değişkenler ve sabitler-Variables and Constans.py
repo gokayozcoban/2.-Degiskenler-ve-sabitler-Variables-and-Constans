a349ca003c8adc0917a9b43de1e187d0d2618d0b
gokayozcoban <43710003+gokayozcoban@users.noreply.github.com>
2020-12-28 22:19:57 +0200

    Add files via upload

---
+++ 2. Değişkenler ve sabitler-Variables and Constans.py
@@ -0,0 +1,22 @@
+# 2. Ders (Değişkenler ve sabitler-Variables and Constans)
+
+isim = "Gökay"
+soyisim = "Özçoban"
+print(isim)
+print(soyisim)
+#Gökay
+#Özçoban
+
+# Değişkenler değiştirilebilir:
+isim,soyisim = soyisim,isim
+print(isim)
+##Özçoban
+
+PI = 3,14 # Constans büyük harfle yazılır ve değiştirilemez. Program içinde de-
+# ğişir ama genel kural olarak değiştirilmemesi tavsiye edildiğinden büyük yazı-
+# lır.
+print(PI)
+#(3, 14)
+PI = 314
+print(PI)
+#314
